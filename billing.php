59478c94950f0f622ca715ecf32bd281e19deb91
Richard <Richard@DESKTOP-Q3EONME>
2024-06-19 13:50:30 +0800

    New Edited

---
+++ billing.php
@@ -0,0 +1,97 @@
+<?php 
+ include 'config.php';
+
+ if(isset($_REQUEST["submit"])){
+ $id = $_REQUEST["id"];
+ $name = $_REQUEST["name"];
+ $date = $_REQUEST["date"];
+ $amount = $_REQUEST["amount"];
+ 
+   $ins = "INSERT INTO billing (id,name,date,amount) VALUES('$id','$name','$date','$amount')";
+   $query = mysqli_query($connection,$ins);
+  }
+ ?>
+<!DOTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+    
+<!-- nav bar start-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!--<a class="navbar-brand" href="dashboard.php"><img src = "img/peak1.png" alt= "PeakPulse Logo"></a>-->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="billing.php">Billing <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Gym profiling
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="gym-profiling.php">Reservation</a>
+          <a class="dropdown-item" href="booking.php">Booking</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="conflict-management.php">Conflict Management</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<!-- nav bar end-->
+
+<!-- form start -->
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Billing ID</label>
+      <input type="text" name= "id"class="form-control" id="inputEmail4" placeholder="Billing ID">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Member Name</label>
+      <input type="text" name= "name" class="form-control" id="inputPassword4" placeholder="Member Name">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="Address">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Billing Date</label>
+    <input type="text" name = "date"class="form-control" id="inputAddress2" placeholder="Billing Date">
+  </div>
+  <div class="form-group">
+      <label for="inputCity">Amount</label>
+      <input type="text" name = "amount"class="form-control" id="inputCity" placeholder = "Amount">
+    </div>
+  <button type="submit" class="btn btn-primary">Confirm</button>
+</form>
+
+<!-- Form end-->
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
+
